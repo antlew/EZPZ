6d894f5d3cec90b684776bf2a1164d58ddd1b995
antlew <anthony.m.lewis5@gmail.com>
2016-05-10 14:43:38 -0400

    EZPZ 5/10

---
+++ src/com/gcproject/ezpz/EZPZMain.java
@@ -0,0 +1,10 @@
+package com.gcproject.ezpz;
+
+public class EZPZMain {
+//	
+//	 public static void main(String[] args) throws Exception {
+//		    EZPZDao dao = new EZPZDao();
+//		    dao.readDataBase();
+//		  }
+
+}
---
+++ src/com/gcproject/ezpz/EZPZServlet.java
@@ -0,0 +1,40 @@
+package com.gcproject.ezpz;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/form")
+public class EZPZServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String meat = request.getParameter("meat");
+		String results = "";
+//		String meat = "chicken";
+		
+		
+		try {
+			results = EZPZDao.getMeals(meat);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(results);
+		
+		
+	}
+
+
+}
